bc3022a6c73287217135d6675295de6c43e1c079
zhugongyi <zhugongyi@niwodai.net>
2018-01-04 09:36:41 +0800

    test batch insert

--- src/main/java/me/zgy/es/ESHandler.java
+++ src/main/java/me/zgy/es/ESHandler.java
@@ -13,6 +13,7 @@ import java.util.*;
 
 /**
  * Created by Rayee on 2017/12/28.
+ * 生成索引名、结果解析
  */
 public class ESHandler {
 
--- src/test/java/me/zgy/test/ESTest.java
+++ src/test/java/me/zgy/test/ESTest.java
@@ -6,10 +6,15 @@ import me.zgy.bean.param.BlackSearchParam;
 import me.zgy.exception.ServiceException;
 import me.zgy.service.es.index.BlackIndexService;
 import me.zgy.service.es.search.BlackIndexSearchService;
+import me.zgy.utils.ChineseName;
 import me.zgy.utils.JsonUtils;
+import me.zgy.utils.test.IdCardGenerator;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,6 +37,43 @@ public class ESTest extends BaseTest {
     public void test02() throws ServiceException {
         List<TBlack> blacks = blackSearchService.query(BlackSearchParam.builder().creditNo("320681190000000000").build());
         System.out.println(JsonUtils.toJson(blacks));
+    }
+
+    @Test
+    public void test03() throws ServiceException, IOException {
+        long start = System.currentTimeMillis();
+        for (int j = 0; j < 600; j++) {
+            long start1 = System.currentTimeMillis();
+            List<TBlack> list = new ArrayList<>(100);
+            for (int i = 0; i < 30000; i++) {
+                list.add(buildTBlack());
+            }
+            blackIndexService.batchIndex(list);
+            list.clear();
+            System.out.println("j = " + j + "3W数据耗时" + (System.currentTimeMillis() - start1) + "ms");
+        }
+        System.out.println("插入2000W数据耗时" + (System.currentTimeMillis() - start) + "ms");
+    }
+
+    IdCardGenerator generator = new IdCardGenerator();
+
+    private TBlack buildTBlack() {
+        TBlack black = new TBlack();
+        black.setCreditNo(generator.generate());
+        black.setChannel("abc");
+        black.setEntityauthdate("2018-01-02");
+        black.setEntityauthcode("code");
+        black.setSeqno("qazwsx");
+        black.setRelation("relation");
+        black.setMobilePhone("13900000000");
+        black.setMemberName(ChineseName.generate());
+        black.setReasonCode("REASON_CODE");
+        black.setRtmsg("message yeyeye");
+        black.setIsblack("是");
+        black.setAddTime(new Date());
+        black.setUpdateTime(new Date());
+        black.setRtcode("RT_CODE");
+        return black;
     }
 
 }
